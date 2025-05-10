6e67c2ef782742e720b67ddba8ef2fdc013d38c1
MagenX <magenx@users.noreply.github.com>
2025-05-10 19:57:03 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -44,24 +44,60 @@ resource "aws_ssm_parameter" "resolver" {
 # # ---------------------------------------------------------------------------------------------------------------------#
 # VPC values outputs
 # # ---------------------------------------------------------------------------------------------------------------------#
+
 output "vpc_id" {
-  description = "VPC_ID"
+  description = "The ID of the VPC"
   value       = aws_vpc.this.id
 }
 
-output "cidr_block" {
-  description = "CIDR_BLOCK"
+output "vpc_arn" {
+  description = "The ARN of the VPC"
+  value       = aws_vpc.this.arn
+}
+
+output "vpc_cidr_block" {
+  description = "The CIDR block of the VPC"
   value       = aws_vpc.this.cidr_block
 }
 
+output "public_subnet_objects" {
+  description = "A list of all public subnets, containing the full objects."
+  value       = aws_subnet.public
+}
+
 output "public_subnets" {
-  description = "PUBLIC_SUBNETS"
-  value       = [for subnet in aws_subnet.public : subnet.id]
+  description = "List of IDs of public subnets"
+  value       = aws_subnet.public[*].id
+}
+
+output "public_subnet_arns" {
+  description = "List of ARNs of public subnets"
+  value       = aws_subnet.public[*].arn
+}
+
+output "public_subnets_cidr_blocks" {
+  description = "List of cidr_blocks of public subnets"
+  value       = compact(aws_subnet.public[*].cidr_block)
+}
+
+output "private_subnet_objects" {
+  description = "A list of all private subnets, containing the full objects."
+  value       = aws_subnet.private
 }
 
 output "private_subnets" {
-  description = "PRIVATE_SUBNETS"
-  value       = [for subnet in aws_subnet.private : subnet.id]
+  description = "List of IDs of private subnets"
+  value       = aws_subnet.private[*].id
+}
+
+output "private_subnet_arns" {
+  description = "List of ARNs of private subnets"
+  value       = aws_subnet.private[*].arn
+}
+
+output "private_subnets_cidr_blocks" {
+  description = "List of cidr_blocks of private subnets"
+  value       = compact(aws_subnet.private[*].cidr_block)
 }
 
 output "resolver" {
