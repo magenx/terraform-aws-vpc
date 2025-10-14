eb89cabd75dc5bb7547e7ab9c8ede30d7a2ecb85
MagenX <magenx@users.noreply.github.com>
2025-10-14 15:09:10 +0200

    fix: Update to provider aws 6

--- s3_endpoint.tf
+++ s3_endpoint.tf
@@ -7,7 +7,7 @@
 # # ---------------------------------------------------------------------------------------------------------------------#
 resource "aws_vpc_endpoint" "s3" {
   vpc_id            = aws_vpc.this.id
-  service_name      = "com.amazonaws.${data.aws_region.current.name}.s3"
+  service_name      = "com.amazonaws.${data.aws_region.current.region}.s3"
   route_table_ids   = [for rt in values(aws_route_table.private) : rt.id]
   vpc_endpoint_type = "Gateway"
   tags = {
